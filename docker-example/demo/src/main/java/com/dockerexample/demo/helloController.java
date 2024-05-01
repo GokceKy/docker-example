12d8faedb442dddaf4aa9185406e609a997b254c
GokceKy <99399762+GokceKy@users.noreply.github.com>
2024-05-01 22:41:58 +0300

    Add files via upload

---
+++ docker-example/demo/src/main/java/com/dockerexample/demo/helloController.java
@@ -0,0 +1,12 @@
+package com.dockerexample.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @RequestMapping("/")
+    public String index(){
+        return "MY frist deneme";
+    }
+}
