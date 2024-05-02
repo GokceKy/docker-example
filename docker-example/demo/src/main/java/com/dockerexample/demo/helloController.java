bcafeb054ed60d72e61718bef9a3c241b87acab2
GokceKy <99399762+GokceKy@users.noreply.github.com>
2024-05-02 13:14:56 +0300

    Update helloController.java

--- docker-example/demo/src/main/java/com/dockerexample/demo/helloController.java
+++ docker-example/demo/src/main/java/com/dockerexample/demo/helloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
     @RequestMapping("/")
     public String index(){
-        return "MY frist deneme";
+        return "MY frist dnnnnnnnnneneme";
     }
 }
